6766891bd748ed4d42dac497849d7cb051707cd6
Daniil <7102771@gmail.com>
2025-05-19 00:52:43 +0300

    DatabaseConnection

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -1,4 +1,19 @@
 package PACKAGE_NAME;
 
 public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/MoscowMetroTables";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "123456";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void testConnection() {
+        try (Connection conn = getConnection()) {
+            System.out.println("Успешное подключение к базе данных!");
+        } catch (SQLException e) {
+            System.err.println("Ошибка подключения: " + e.getMessage());
+        }
+    }
 }
